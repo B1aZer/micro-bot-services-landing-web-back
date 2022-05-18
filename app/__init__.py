a4bbd02e855d4a114eac3f6cd27b0d228ef6028c
concave-dash <kwerkee12@gmail.com>
2022-05-18 00:59:57 -0400

    create app folder for wsgi

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import render_template, Flask, jsonify, request, make_response
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/pass", methods=['POST'])
+def hello_world():
+    #print(request.json['pass'])
+    resp = make_response(jsonify({"status":"ok"}))
+    resp.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return resp
