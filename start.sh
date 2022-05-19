146ab4a2d7d762c3a8d6e8357cb72c9ee88827b2
concave-dash <kwerkee12@gmail.com>
2022-05-19 15:01:07 -0400

    update discord uri

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ cp ../landing-bootstrap-front/assets/js/bundle-min.js ./app/static/assets/js/
 cp ../landing-bootstrap-front/index.html ./app/templates/
 sed -i 's/assets\//static\/assets\//g' ./app/templates/index.html
 sed -i 's/\/js\/index.js/\/js\/bundle-min.js/g' ./app/templates/index.html
+sed -i 's/redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F/redirect_uri=https%3A%2F%2Fgoodee.finance/g' ./app/templates/index.html
 python server.py
