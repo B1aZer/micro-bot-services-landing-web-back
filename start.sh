b7c4dc9daa7e1cf6d989a6068b0f68b7c331c50b
concave-dash <kwerkee12@gmail.com>
2022-05-19 14:37:29 -0400

    start script auto copy

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 source venv/bin/activate
+cp ../landing-bootstrap-front/assets/css/starter.css ./app/static/assets/css/
+cp ../landing-bootstrap-front/assets/css/starter.css.map ./app/static/assets/css/
+cp ../landing-bootstrap-front/assets/js/bundle-min.js ./app/static/assets/js/
+cp ../landing-bootstrap-front/index.html ./app/templates/
+sed -i 's/assets\//static\/assets\//g' ./app/templates/index.html
+sed -i 's/\/js\/index.js/\/js\/bundle-min.js/g' ./app/templates/index.html
 python server.py
